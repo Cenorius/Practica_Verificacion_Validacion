f66aa6d16beec28af0791dc57ed34677272ad739
cenorius <dave.ayuso@gmail.com>
2017-03-14 16:59:34 +0100

    Fixed accents 	modified:   Practica/practica.py

--- Practica/practica.py
+++ Practica/practica.py
@@ -1,7 +1,10 @@
+import unicodedata
 
-def count(stra):
 
-	la_words=stra.split(" ")
+def count(ustra):
+	ustra=unicodedata.normalize('NFKD', ustra).encode('ASCII', 'ignore')
+
+	la_words=ustra.split(" ")
 	la_frecuencies=[]
 
 	for w in la_words:
@@ -10,6 +13,6 @@ def count(stra):
 	lcount=zip(la_words, la_frecuencies)
 	
 	lcount=sorted(set(lcount),key=lambda x:x[1])
-
+	
 	return lcount
 
