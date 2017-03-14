a4e87c12c5e68e22581dee7cb94edb0fb0228eb1
cenorius <dave.ayuso@gmail.com>
2017-03-14 11:49:14 +0100

    Add count words test 	modified:   tests/test_practica.py

--- tests/test_practica.py
+++ tests/test_practica.py
@@ -1,1 +1,15 @@
+# -*- coding: utf-8 -*-
 
+from .context import Practica
+
+import unittest
+
+class PracticaTestSuite(unittest.TestCase):
+
+	def test_count(self):
+		
+		str="si si Hola bien bien bien"
+		
+		result=Practica.count(str)
+		
+		self.assertEqual(result,[('Hola',1),('si',2),('bien',3)],"La lista devuelta no es correcta")
