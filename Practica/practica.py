810cc7de5372390d616124dc7c1611a0a778418d
cenorius <dave.ayuso@gmail.com>
2017-03-14 11:50:42 +0100

    Modify practica code to count words test 	modified:   Practica/practica.py

--- Practica/practica.py
+++ Practica/practica.py
@@ -1,1 +1,15 @@
 
+def count(stra):
+
+	la_words=stra.split(" ")
+	la_frecuencies=[]
+
+	for w in la_words:
+		la_frecuencies.append(la_words.count(w))
+
+	lcount=zip(la_words, la_frecuencies)
+	
+	lcount=sorted(set(lcount),key=lambda x:x[1])
+
+	return lcount
+
