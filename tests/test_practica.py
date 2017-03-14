556aa7d0d9863b0580e4b7cf137821239f9896a3
cenorius <dave.ayuso@gmail.com>
2017-03-14 18:48:13 +0100

    Add test to removeStopWords 	modified:   tests/test_practica.py

--- tests/test_practica.py
+++ tests/test_practica.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-
 from .context import Practica
 import unicodedata
+
 
 import unittest
 
@@ -9,34 +9,33 @@ class PracticaTestSuite(unittest.TestCase):
 
 	def test_count(self):
 		
-		str=u"si si hola bien bien bien"
+		str=u"bicicleta bicicleta hola bien bien bien"
 		
 		result=Practica.count(str)
 		
-		self.assertEqual(result,[('hola',1),('si',2),('bien',3)],"La lista devuelta no es correcta")
+		self.assertEqual(result,[('hola',1),('bicicleta',2),('bien',3)],"La lista devuelta no es correcta")
 
 	def test_count_acents(self):
 
-		str=u'si sí hola bièn bién bien'
+		str=u'bicicleta bicicletá hola bièn bién bien'
 		
 		result=Practica.count(str)
 		
-		self.assertEqual(result,[('hola',1),('si',2),('bien',3)],"La lista devuelta tiene acentos")
+		self.assertEqual(result,[('hola',1),('bicicleta',2),('bien',3)],"La lista devuelta tiene acentos")
 
 	def test_count_upper(self):
 
-		str=u'Si si Hola Bien biEn bieN BIEN'
+		str=u'Bicicleta bicicleta Hola Bien biEn bieN BIEN'
 
 		result=Practica.count(str)
 		
-		self.assertEqual(result,[('hola',1),('si',2),('bien',4)],"La lista devuelta tiene mayusculas")
+		self.assertEqual(result,[('hola',1),('bicicleta',2),('bien',4)],"La lista devuelta tiene mayusculas")
 
 	def test_removeStopwords(self):
-		stopwords=['la','me','el']
-		
-		str=['el','la','me','the','je','of','coche']
 
-		result=Practica.removeStopwords(str,stopwords)
+		lstr=['yo','el','ella','tuyos','como','coche']
+
+		result=Practica.removeStopwords(lstr)
 
 		print(result)
 		self.assertEqual(result,['coche'],"La lista devuelta tiene stopwords")
