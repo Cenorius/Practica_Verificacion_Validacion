731c07f34c61646aa0108ad4651fcafb6ae1fcac
cenorius <dave.ayuso@gmail.com>
2017-03-14 17:08:33 +0100

    Add uppercase test 	modified:   tests/test_practica.py

--- tests/test_practica.py
+++ tests/test_practica.py
@@ -9,16 +9,24 @@ class PracticaTestSuite(unittest.TestCase):
 
 	def test_count(self):
 		
-		str=u"si si Hola bien bien bien"
+		str=u"si si hola bien bien bien"
 		
 		result=Practica.count(str)
 		
-		self.assertEqual(result,[('Hola',1),('si',2),('bien',3)],"La lista devuelta no es correcta")
+		self.assertEqual(result,[('hola',1),('si',2),('bien',3)],"La lista devuelta no es correcta")
 
 	def test_count_acents(self):
 
-		str=u'si sí Hola bièn bién bien'
+		str=u'si sí hola bièn bién bien'
 		
 		result=Practica.count(str)
 		
-		self.assertEqual(result,[('Hola',1),('si',2),('bien',3)],"La lista devuelta tiene acentos")
+		self.assertEqual(result,[('hola',1),('si',2),('bien',3)],"La lista devuelta tiene acentos")
+
+	def test_count_upper(self):
+
+		str=u'Si si Hola Bien biEn bieN BIEN'
+
+		result=Practica.count(str)
+		print(result)
+		self.assertEqual(result,[('hola',1),('si',2),('bien',4)],"La lista devuelta tiene mayusculas")
