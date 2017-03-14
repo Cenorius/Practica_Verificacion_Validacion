54cd4567817663e088f78d0daae3e814448453e0
cenorius <dave.ayuso@gmail.com>
2017-03-14 16:58:43 +0100

    Add test for accents 	modified:   tests/test_practica.py

--- tests/test_practica.py
+++ tests/test_practica.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from .context import Practica
+import unicodedata
 
 import unittest
 
@@ -8,8 +9,16 @@ class PracticaTestSuite(unittest.TestCase):
 
 	def test_count(self):
 		
-		str="si si Hola bien bien bien"
+		str=u"si si Hola bien bien bien"
 		
 		result=Practica.count(str)
 		
 		self.assertEqual(result,[('Hola',1),('si',2),('bien',3)],"La lista devuelta no es correcta")
+
+	def test_count_acents(self):
+
+		str=u'si sí Hola bièn bién bien'
+		
+		result=Practica.count(str)
+		
+		self.assertEqual(result,[('Hola',1),('si',2),('bien',3)],"La lista devuelta tiene acentos")
