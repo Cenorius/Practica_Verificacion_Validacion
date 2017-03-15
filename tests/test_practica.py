3a43d6f0685d0e3b8c1c69e44ca17696458953c6
cenorius <dave.ayuso@gmail.com>
2017-03-15 18:46:15 +0100

    Add tests to getWordsFrecuencies 	modified:   tests/test_practica.py

--- tests/test_practica.py
+++ tests/test_practica.py
@@ -62,11 +62,64 @@ class PracticaTestSuite(unittest.TestCase):
 		
 		self.assertEqual(result,["que" ,"pasa" ,"tio","colega","bien","bien"],"La lista devuelta tiene simbolos")
 
-	def test_getWordsFrecuencyNone(self):
-		lstr=None
+	def test_getWordsFrecuency(self):
+		lstr=['hola','bicicleta','bicicleta','bien','bien','bien','bien']
 
 		result=Practica.getWordsFrecuencies(lstr)
 		print(result)
-		self.assertEqual(result, [], "No se ha devuelto un None")
+		self.assertEqual(result,[('hola',1),('bien',4),('bicicleta',2)],"La lista devuelta no es correcta")
 
+	def test_getWordsFrecuency_None(self):
+		lstr=None
 
+		with self.assertRaises(TypeError):
+			Practica.getWordsFrecuencies(lstr)
+
+	def test_getWordsFrecuency_Integers(self):
+		lstr=[1,2,3]
+
+		with self.assertRaises(TypeError):
+			Practica.getWordsFrecuencies(lstr)
+
+	def test_getWordsFrecuency_String_Integers(self):
+		lstr=["test",1,2,3]
+
+		with self.assertRaises(TypeError):
+			Practica.getWordsFrecuencies(lstr)
+
+	def test_getWordsFrecuency_Strings_Integer(self):
+		lstr=["test","test2",3]
+
+		with self.assertRaises(TypeError):
+			Practica.getWordsFrecuencies(lstr)
+
+	def test_getWordsFrecuency_Unicodes_Integer(self):
+		lstr=[u"test",u"test2",3]
+
+		with self.assertRaises(TypeError):
+			Practica.getWordsFrecuencies(lstr)
+
+	def test_getWordsFrecuency_Unicode_Integers(self):
+		lstr=[u"test",1,2,3]
+
+		with self.assertRaises(TypeError):
+			Practica.getWordsFrecuencies(lstr)
+	
+	def test_getWordsFrecuency_Empty(self):
+		lstr=[]
+
+		result=Practica.getWordsFrecuencies(lstr)
+
+		self.assertEqual(result,[],"La lista devuelta no esta vacia")
+
+	def test_getWordsFrecuency_Unicode_Integer(self):
+		lstr=1
+
+		with self.assertRaises(TypeError):
+			Practica.getWordsFrecuencies(lstr)
+
+	def test_getWordsFrecuency_SpecialCharacter(self):
+		lstr="©"
+
+		with self.assertRaises(TypeError):
+			Practica.getWordsFrecuencies(lstr)
